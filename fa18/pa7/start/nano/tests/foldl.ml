1692cb5eb2705f1dcc81eb85a9e92ddcfa5f88c5
John Sarracino <jsarraci@cs.ucsd.edu>
2018-11-27 15:00:55 -0800

    final pass

--- fa18/pa7/start/nano/tests/foldl.ml
+++
@@ -1,7 +0,0 @@
-let foldl = fun f -> fun b -> fun l -> 
-  let rec worker = fun acc -> fun xs ->
-    if (null xs) then acc else worker (f acc (hd xs)) (tl xs)
-  in
-    worker b l
-in
-  foldl
--- fa18/pa7/start/nano/tests/map.ml
+++
@@ -1,5 +0,0 @@
-let rec map = 
-  fun f -> fun xs ->
-    if (null xs) then [] else (f (hd xs)) :: (map f (tl xs))
-in
-  map
